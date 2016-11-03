1c6e8ba0a218e0887a572a6bf137f5038efb1d53
manugarciaquismondo <mgarcia.universidad@gmail.com>
2016-11-03 12:17:54 -0400

    Reproductive number tunned

--- matlab/InputValues.m
+++ matlab/InputValues.m
@@ -1,8 +1,12 @@
 base_nu=.001;
-rotavirusReproductiveNumber=30;
+rotavirusReproductiveNumber=40;
 simulatedYears=2500;
 imageRoute='results';
 parametersFileName='results';
 productionIterationFunction=@BaselineLambda;
 Rotaviruscode12_15_2015
+
+% base_nu=.001;rotavirusReproductiveNumber=40;simulatedYears=1200;imageRoute='results';parametersFileName='results';Rotaviruscode12_15_2015
+
+
 
