e2de4f5cd21758fa9cde0cddf78fac20fbc7085f
manugarciaquismondo <mgarcia.universidad@gmail.com>
2016-11-02 21:05:47 -0400

    Added file with input values

---
+++ matlab/InputValues.m
@@ -0,0 +1,8 @@
+base_nu=.001;
+rotavirusReproductiveNumber=30;
+simulatedYears=2500;
+imageRoute='results';
+parametersFileName='results';
+productionIterationFunction=@BaselineLambda;
+Rotaviruscode12_15_2015
+
