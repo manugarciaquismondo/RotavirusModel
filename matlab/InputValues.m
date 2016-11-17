f8c45f11a16032defb83e963c5dcd5632011f3c1
manugarciaquismondo <mgarcia.universidad@gmail.com>
2016-11-17 16:23:25 -0500

    Parameters retuned

--- matlab/InputValues.m
+++ matlab/InputValues.m
@@ -1,13 +1,13 @@
 base_nu=.001;
-rotavirusReproductiveNumber=40;
-simulatedYears=200; % 2500 for generation of stable values
+rotavirusReproductiveNumber=20.85; %With 21 we get 3.6%, that is acceptable
+simulatedYears=100; % 2500 for generation of stable values
 scenarioName='exaggerationandwarming';
 productionIterationFunction=@ExaggerationAndWarmingLambda; % Default is @BaselineLambda
 finalValues=strcat('simulationResults',scenarioName);
 seriesFileName=strcat('seriesResults',scenarioName);
 parametersFileName=strcat('parameters', scenarioName);
 imageFileName=scenarioName;
-startValues='burninAltData';
+startValues='data';
 Rotaviruscode12_15_2015
 
 % base_nu=.001;rotavirusReproductiveNumber=40;simulatedYears=1200;imageRoute='results';parametersFileName='results';Rotaviruscode12_15_2015
